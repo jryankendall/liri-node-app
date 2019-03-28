3535f3041aa2c725da758c9ce9ad9dfd3a09544f
unknown <jed@jedkendall.com>
2019-03-28 18:16:23 -0500

    Implemented first 2 APIs

--- liri.js
+++ liri.js
@@ -21,7 +21,71 @@ if (argV.length >= 5) {
     }
 }
 
+//Setting up global vars
+var searchTerm = "";
+var cLog = console.log;
+
 setTimeout(function() {
-    console.log("Command: " + commandArg + "\nQuery Arguments: " + queryArg);
+    cLog("Command: " + commandArg + "\nQuery Arguments: " + queryArg);
 }, 100)
 
+function getConcerts(artist) {
+    axios({
+        method : 'get',
+        url : "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    }).then(function(response){
+        var data = response.data;
+        cLog("You Searched For: " + artist + "\nResults:");
+        for (var x in data) {
+            var concertTime = moment(data[x].datetime, "YYYY-MM-DDTHH:mm:ss");
+            var concertVenue = data[x].venue;
+            var concertLineup = data[x].lineup.join(', ');
+            cLog("\n" + (parseInt(x)+1) + ". " + concertLineup);
+            cLog("Venue: " + concertVenue.name + " in " + concertVenue.city + ", " + concertVenue.region + " " + concertVenue.country);
+            cLog("Date: " + concertTime.format("dddd, MMMM Do YYYY"));
+        }
+    }).catch(function(error) {
+        cLog("Something went wrong\n------");
+        cLog(error);
+        cLog("-----");
+    });
+}
+
+function spotifyThis(search) {
+    spotify.search({ type: 'track', query: search, limit: 1}, function(error, response) {
+        if (error) {
+            return cLog(error);
+        }
+
+        var data = response.tracks.items;
+        cLog(response.tracks.items);
+    })
+}
+
+if (commandArg == undefined) {
+    cLog("Invalid arguments received. Try again.");
+    return false;
+} else {
+    switch (commandArg) {
+        case "concert-this":
+            getConcerts(queryArg);
+            break;
+        case "spotify-this-song":
+            if (queryArg == undefined) {
+                spotifyThis("the sign");
+            } else 
+            {
+                spotifyThis(queryArg);
+            }
+            break;
+        case "movie-this":
+            movieThis(queryArg);
+            break;
+        case "do-what-it-says":
+            doThis(queryArg);
+            break;
+        default:
+            cLog("Invalid command. Valid commands are: \nconcert-this\nspotify-this-song\nmovie-this\ndo-what-it-says\n--End of Line--\n");
+            break;
+    }
+}
